4499c01a46484d25a60987c3a24b3cc36f272615
jjpark51 <jjpark57@hotmail.com>
2022-12-10 01:42:32 +0900

    Implemented the see more recipe page/ profile page

---
+++ static/profile.js
@@ -0,0 +1,63 @@
+function myRecipe() {
+
+    $(".recipes").empty()
+
+    let recipeBox = document.getElementById("recipes");
+
+    let recipeImage = document.createElement("div")
+    recipeImage.setAttribute("class", "recipeImage")
+
+
+    let recipeType = document.createElement("div")
+    recipeType.setAttribute("class", "recipeType")
+
+    let recipeName = document.createElement("h2")
+    recipeName.innerHTML = "Cabbage Greens"
+
+    let recipeInstruction = document.createElement("p")
+    recipeInstruction.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aperiam molestias ratione tenetur voluptatum expedita sint, quaerat facere tempora amet officiis maiores eaque pariatur dolorum reiciendis? Obcaecati maiores possimus quas.";
+
+    let line = document.createElement("div")
+    line.setAttribute("class", "line")
+
+    recipeType.appendChild(recipeName)
+    recipeType.appendChild(recipeInstruction)
+
+    recipeBox.appendChild(recipeImage)
+    recipeBox.appendChild(recipeType)
+    recipeBox.appendChild(line)
+
+
+
+}
+
+function favRecipe() {
+
+    $('.recipes').empty()
+
+    let recipeBox = document.getElementById("recipes");
+
+    let recipeImage = document.createElement("div")
+    recipeImage.setAttribute("class", "recipeImage")
+
+
+    let recipeType = document.createElement("div")
+    recipeType.setAttribute("class", "recipeType")
+
+    let recipeName = document.createElement("h2")
+    recipeName.innerHTML = "Bananan Smoothie"
+
+    let recipeInstruction = document.createElement("p")
+    recipeInstruction.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aperiam molestias ratione tenetur voluptatum expedita sint, quaerat facere tempora amet officiis maiores eaque pariatur dolorum reiciendis? Obcaecati maiores possimus quas.";
+
+    let line = document.createElement("div")
+    line.setAttribute("class", "line")
+
+    recipeType.appendChild(recipeName)
+    recipeType.appendChild(recipeInstruction)
+
+    recipeBox.appendChild(recipeImage)
+    recipeBox.appendChild(recipeType)
+    recipeBox.appendChild(line)
+
+}
