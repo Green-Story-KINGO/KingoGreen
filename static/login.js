1a34ad52c0849c15d9e15b3c6f150dd2b7465031
jjpark51 <jjpark57@hotmail.com>
2022-12-10 18:44:38 +0900

    added images and login page

---
+++ static/login.js
@@ -0,0 +1,320 @@
+let counter = 0
+let gender = 0;
+let UserFirstName = ''
+let UserLastName = ''
+let UserGender = 0
+let UserEmail = ''
+let UserPassword = ''
+let correct = 0
+let loginCorrect = 0
+
+
+function passwordCheck(str) { // Here this function checks to see if th password has correct format
+    // Regex to check if a string
+    // contains uppercase, lowercase
+    // special character & numeric value
+    let pattern = new RegExp( // https://bobbyhadz.com/blog/javascript-check-if-string-contains-character
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$" // This is where we check if there are any special characters in
+    );
+
+    // If the string is empty
+    // then print No
+    if (!str || str.length === 0) {
+      return 0;
+    }
+
+    // Print Yes If the string matches
+    // with the Regex
+    if (pattern.test(str)) {
+        return 1
+    } else {
+        return 0
+    }
+  }
+
+  function firstLetterCap(str) { // This checks to see if the first character is a capital letter
+    if(str[0].toUpperCase() === str[0]){ // https://bobbyhadz.com/blog/javascript-check-if-string-contains-character
+        return 1
+    }
+    return 0
+  }
+
+  function containNumber(str) { // This checks to see if the irst letter is a capital letter
+    if (/[0-9]/.test(str)) { // https://bobbyhadz.com/blog/javascript-check-if-string-contains-character
+        return 0
+    }
+    return 1
+  }
+
+  function specialCharacterCheck(str){ // This checks to see if there are any special characters
+    if(/^[a-zA-Z0-9 ]*$/.test(str)) { // https://bobbyhadz.com/blog/javascript-check-if-string-contains-character
+        return 1;
+    }
+    return 0
+  }
+ 
+$('.loginPage').click(function() { // This loads the login page
+
+    $('.container').fadeOut(200);
+    $('.container').fadeIn(1000)
+
+    if (counter> 0) {
+        $(".topPage").empty(); // This checks to see if the page has already been visited
+        $(".info").empty();
+        $(".buttonPlace").empty();
+
+    }
+    counter++; // This indicates the page has been visited
+    $('.loginPage').prop('disabled', true) // This disables the other tab
+    $('.signPage').prop('disabled', false)
+
+
+    $('.container').css({height: '350px'});
+
+    let loginHead = $('<h2></h2>').text("Login")
+
+    loginHead.css({color:'black'});
+
+    let loginInfo = $('<a></a>').text("Enter user name and password:")
+    loginInfo.css('color', 'black')
+
+    $('.topPage').append(loginHead)
+    $('.topPage').append(loginInfo)
+
+
+    // This is where we put the login info
+
+    let email = $('<p></p>').text("Username")
+    email.css({'color': 'black', 'font-size': '10pt' , 'width': '100%'})
+    let emailBox = $('<input type=text></input>')
+    emailBox.attr('placeholder', 'Username')
+    emailBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+
+    $('.info').append(email)
+    $('.info').append(emailBox)
+
+    let password = $('<p></p>').text("Password")
+    password.css({'color': 'black', 'font-size': '10pt', 'width': '100%' })
+    let passwordBox = $('<input type=text></input>')
+    passwordBox.attr('placeholder', 'Password')
+    passwordBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+
+    $('.info').append(password)
+    $('.info').append(passwordBox)
+
+    let loginButton = $('<button></button>')
+    loginButton.text("Login")
+    loginButton.css({ 'text-align':'center', 'color':'black', 'width': '100%', 'height': '40px','background-color': 'green'})
+
+    $('.buttonPlace').append(loginButton)
+    loginButton.click(function(){
+        console.log(emailBox.val())
+        console.log(passwordBox.val())
+        console.log(UserEmail)
+        console.log(UserPassword)
+        if(emailBox.val().length > 0 && passwordBox.val().length > 0){
+          console.log("in")
+        
+            if(emailBox.val() == UserEmail && passwordBox.val() == UserPassword){ // Validates the email and password
+                $('.container').css({height: '100px'});
+
+            
+                $(".topPage").empty();
+                $(".info").empty();
+                $(".buttonPlace").empty();
+
+                let loginHead = $('<h2></h2>').text("You are logged in.")
+
+                loginHead.css({color:'black'});
+                $('.topPage').append(loginHead)
+            }
+            else {
+                loginInfo.text("Credentials do not match!")
+                loginInfo.css('color', 'red')
+
+
+            }
+        }
+
+    })
+
+
+    //Now we are going to create functions to check conditions
+    let emailValue = emailBox.val()
+    console.log(typeof emailValue)
+    console.log(emailValue.length)
+
+      
+
+})
+
+
+
+$('.signPage').click(function() { // This brings us to the sign up page
+  $('.container').fadeOut(200);
+  $('.container').fadeIn(1000) // This fades in and fades out the page
+
+    if (counter> 0) {
+        $(".topPage").empty();
+        $(".info").empty();
+        $(".buttonPlace").empty();
+
+    }
+    counter++;
+    let clicked = 0
+
+    $('.loginPage').prop('disabled', false)
+    $('.signPage').prop('disabled', true)
+    
+
+    $('.container').css({height: '530px'});
+
+    let signHead = $('<h2></h2>').text("Sign up")
+
+    signHead.css({color:'black'});
+
+    let signInfo = $('<a></a>').text("Fill in the form:")
+    signInfo.css('color', 'black')
+
+    $('.topPage').append(signHead)
+    $('.topPage').append(signInfo)
+
+
+    // This is where we put the login info
+    let firstName = $('<p></p>').text("Username")
+    firstName.css({'color': 'black', 'font-size': '10pt' , 'width': '100%'})
+    let firstBox = $('<input type=text></input>')
+    firstBox.attr('placeholder', 'Username')
+    firstBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+
+    
+    $('.info').append(firstName)
+    $('.info').append(firstBox)
+    
+     
+
+    let email = $('<p></p>').text("Email")
+    email.css({'color': 'black', 'font-size': '10pt' , 'width': '100%'})
+    let emailBox = $('<input type=text></input>')
+    emailBox.attr('placeholder', 'Email')
+    emailBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+
+    $('.info').append(email)
+    $('.info').append(emailBox)
+
+
+
+    let password = $('<p></p>').text("Password")
+    password.css({'color': 'black', 'font-size': '10pt', 'width': '100%' })
+    let passwordBox = $('<input type=text></input>')
+    passwordBox.attr('placeholder', 'Password')
+    passwordBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+
+    $('.info').append(password)
+    $('.info').append(passwordBox)
+    
+    let confPassword = $('<p></p>').text("Confirm Password")
+    confPassword.css({'color': 'black', 'font-size': '10pt', 'width': '100%' })
+    let confBox = $('<input type=text></input>')
+    confBox.attr('placeholder', 'Confirm Password')
+    confBox.css({'color': ' #384047', 'background-color': 'whitesmoke' , 'border': '1px solid black', 'width': '100%'})
+   
+    $('.info').append(confPassword)
+    $('.info').append(confBox)
+
+
+    let SignButton = $('<button></button>')
+    SignButton.text("Sign Up")
+    SignButton.css({'background-color':'green', 'text-align':'center', 'color':'black', 'width': '100%', 'height': '40px'})
+
+    $('.buttonPlace').append(SignButton)
+
+    SignButton.click(function(){ // This is the sign up  button
+
+        if(correct >= 6){ // If enough correct happened then we sign in
+        $('.container').css({height: '100px'});
+
+      
+            $(".topPage").empty();
+            $(".info").empty();
+            $(".buttonPlace").empty();
+
+            let signHead = $('<h2></h2>').text("You are signed up.")
+    
+            signHead.css({color:'black'});
+            $('.topPage').append(signHead)
+    
+        }
+        if(firstBox.val().length === 0){ 
+          firstBox.css('border', '1px solid red')
+                
+                
+        
+          firstName.append(firstNameWarning)
+          firstNameOk.remove()
+          firstNameWarning.text("Please enter your first name")
+          firstNameWarning.css({'position':'relative', 'top':'30px', 'left':'230px'})
+        }
+        if(lastBox.val().length === 0){
+          lastBox.css('border', '1px solid red')
+                
+                
+        
+          lastName.append(lastNameWarning)
+          lastNameOk.remove()
+          lastNameWarning.text("Please enter your last name")
+          lastNameWarning.css({'position':'relative', 'top':'30px', 'left':'230px'})
+        }
+
+        if(emailBox.val().length === 0){
+          emailBox.css('border', '1px solid red')
+                
+                
+        
+          email.append(emailWarning)
+          emailOk.remove()
+          emailWarning.text("Please enter your email")
+          emailWarning.css({'position':'relative', 'top':'30px', 'left':'250px'})
+        }
+        if(passwordBox.val().length === 0){
+          passwordBox.css('border', '1px solid red')
+                
+                
+        
+          password.append(passwordWarning)
+          passwordOk.remove()
+          passwordWarning.text("Please enter your password")
+          passwordWarning.css({'position':'relative', 'top':'30px', 'left':'230px'})
+        }
+        if(confBox.val().length === 0){
+          confBox.css('border', '1px solid red')
+                
+                
+        
+          confPassword.append(confirmWarning)
+          confirmOk.remove()
+          confirmWarning.text("Please enter your password")
+          confirmWarning.css({'position':'relative', 'top':'30px', 'left':'180px'})
+        }
+
+     
+
+
+        if(clicked == 0) {
+
+          Foption.append(genderWarning)
+          genderOk.remove()
+
+
+
+        }
+        correct = 0
+
+    })
+
+
+    // Now we are going to check the conditions of each
+
+})
+
+
